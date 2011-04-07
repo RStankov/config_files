82058f0fbc5c42809008a2a6f7d87c8c684fe090
Radoslav Stankov <rstankov@gmail.com>
2011-04-07 22:03:32 +0300

    Moved Installer into separate folder.

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,3 +1,5 @@
+require 'lib/installer.rb'
+
 task :default => [:install]
 
 desc "install config files into user's home directory"
@@ -10,66 +12,13 @@ task :install do
     Installer.link file_name
   end
 
+  text_mate_path = "#{ENV['HOME']}/Library/Application Support/TextMate"
+
   Dir['textmate/bundles/*'].each do |file_name|
-    Installer.link file_name, :directory => "#{Installer::TEXTMATE_PATH}/Bundles"
+    Installer.link file_name, :directory => "#{text_mate_path}/Bundles"
   end
 
   Dir['textmate/plugins/*'].each do |file_name|
-    Installer.link file_name, :directory => "#{Installer::TEXTMATE_PATH}/Plugins"
-  end
-end
-
-class Installer
-  TEXTMATE_PATH = "#{ENV['HOME']}/Library/Application Support/TextMate"
-
-  class << self
-    def link(file_name, options = {})
-      link = self.new(file_name, options)
-
-      if link.already_exists?
-        link.propose_overwrite
-      else
-        link.create
-      end
-    end
-  end
-
-  def initialize(file_name, options = {})
-    @file_name = file_name
-    @link_name = options.delete(:link_name) || File.basename(file_name)
-    @directory = options.delete(:directory) || ENV['HOME']
-  end
-
-  def already_exists?
-    File.exist? link_path
-  end
-
-  def propose_overwrite
-    puts "overwrite #{link_path} ? [ynq] "
-    case $stdin.gets.chomp
-      when 'y' then replace
-      when 'n' then puts "skipping #{link_path}"
-      when 'q' then exit
-    end
-  end
-
-  def replace
-    system "rm #{e link_path}"
-    create
-  end
-
-  def create
-    puts "linking #{link_path}"
-    system "ln -s $PWD/#{e @file_name} #{e link_path}"
-  end
-
-  private
-
-  def link_path
-    File.join(@directory, @link_name)
-  end
-
-  def e(path)
-    path.gsub(' ', '\ ')
+    Installer.link file_name, :directory => "#{text_mate_path}/Plugins"
   end
 end
---
+++ lib/installer.rb
@@ -0,0 +1,52 @@
+class Installer
+  class << self
+    def link(file_name, options = {})
+      link = self.new(file_name, options)
+
+      if link.already_exists?
+        link.propose_overwrite
+      else
+        link.create
+      end
+    end
+  end
+
+  def initialize(file_name, options = {})
+    @file_name = file_name
+    @link_name = options.delete(:link_name) || File.basename(file_name)
+    @directory = options.delete(:directory) || ENV['HOME']
+  end
+
+  def already_exists?
+    File.exist? link_path
+  end
+
+  def propose_overwrite
+    puts "overwrite #{link_path} ? [ynq] "
+    case $stdin.gets.chomp
+      when 'y' then replace
+      when 'n' then puts "skipping #{link_path}"
+      when 'q' then exit
+    end
+  end
+
+  def replace
+    system "rm #{e link_path}"
+    create
+  end
+
+  def create
+    puts "linking #{link_path}"
+    system "ln -s $PWD/#{e @file_name} #{e link_path}"
+  end
+
+  private
+
+  def link_path
+    File.join(@directory, @link_name)
+  end
+
+  def e(path)
+    path.gsub(' ', '\ ')
+  end
+end
