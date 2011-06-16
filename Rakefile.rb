64b01964d0fe115adfa08f8c146bc64ce4fdf246
Radoslav Stankov <rstankov@gmail.com>
2011-06-16 15:22:52 +0300

    Split textmate config into bundles and preferences

--- Rakefile.rb
+++ Rakefile.rb
@@ -16,28 +16,37 @@ namespace :install do
   end
 
   desc 'Configurate textmate'
-  task :textmate do
-    text_mate_path = "#{ENV['HOME']}/Library/Application Support/TextMate"
+  task :textmate => ['textmate:bundles', 'textmate:preferences']
 
-    %w[bundles plugins themes].each do |component|
-      Installer.link_dir_contents "textmate/#{component}", "#{text_mate_path}/#{component.capitalize}"
+  namespace :textmate do
+    desc 'Configurate textmate bundles'
+    task :bundles do
+      text_mate_path = "#{ENV['HOME']}/Library/Application Support/TextMate"
+      %w[bundles plugins themes].each do |component|
+        Installer.link_dir_contents "textmate/#{component}", "#{text_mate_path}/#{component.capitalize}"
+      end
+
+      system %Q[osascript -e 'tell app "TextMate" to reload bundles']
     end
 
-    system %Q[osascript -e 'tell app "TextMate" to reload bundles']
-
-    Installer.defaults 'com.macromates.textmate', {
-      'OakFolderReferenceFolderPattern'   => "-string '!.*/(\\.[^/]*|CVS|tmp|log|autotest|script|public|tmp|_darcs|_MTN|\\{arch\\}|blib|.*~\\.nib|.*\\.(framework|app|pbproj|pbxproj|xcode(proj)?|bundle))$'",
-      'OakTextViewLineNumbersEnabled'     => 1,
-      'OakTextViewShowInvisiblesEnabled'  => 1,
-      'OakWordCharacters'                 => "'-_'",
-      'OakProjectDrawerPrefersRightEdge'  => 1,
-      'OakProjectDrawerWidth'             => 300,
-      'OakThemeManagerSelectedTheme'      => "'A4299D9B-1DE5-4BC4-87F6-A757E71B1597'",
-      'OakShellVariables'                 => '-array ' + [
-        '{enabled = 1; value = "PATH"; variable = "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin";}',
-        '{enabled = 1; value = "TM_GIT"; variable = "git";}',
-        '{enabled = 1; value = "TM_RUBY"; variable = "ruby";}'
-      ].map { |v| "'#{v}'" }.join(' ')
-    }
+    desc 'Configurate textmate preferences'
+    task :preferences do
+      Installer.defaults 'com.macromates.textmate', {
+        'OakFolderReferenceFolderPattern'   => "-string '!.*/(\\.[^/]*|CVS|tmp|log|autotest|script|public|tmp|_darcs|_MTN|\\{arch\\}|blib|.*~\\.nib|.*\\.(framework|app|pbproj|pbxproj|xcode(proj)?|bundle))$'",
+        'OakTextViewLineNumbersEnabled'     => 1,
+        'OakTextViewShowInvisiblesEnabled'  => 1,
+        'OakWordCharacters'                 => "'-_'",
+        'OakProjectDrawerPrefersRightEdge'  => 1,
+        'OakProjectDrawerWidth'             => 300,
+        'OakThemeManagerSelectedTheme'      => "'A4299D9B-1DE5-4BC4-87F6-A757E71B1597'",
+        'OakTextViewNormalFontName'         => "-string 'Inconsolata'",
+        'OakTextViewNormalFontSize'         => '16',
+        'OakShellVariables'                 => '-array ' + [
+          '{enabled = 1; value = "PATH"; variable = "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin";}',
+          '{enabled = 1; value = "TM_GIT"; variable = "git";}',
+          '{enabled = 1; value = "TM_RUBY"; variable = "ruby";}'
+        ].map { |v| "'#{v}'" }.join(' ')
+      }
+    end
   end
 end
