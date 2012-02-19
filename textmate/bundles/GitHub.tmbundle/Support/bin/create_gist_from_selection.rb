ac9178b5a1f407c69ce60794796c6da23002308e
Radoslav Stankov <rstankov@gmail.com>
2012-02-19 15:19:36 +0200

    Use submodule for GitHub Textmate bundle

--- textmate/bundles/GitHub.tmbundle/Support/bin/create_gist_from_selection.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-$:.unshift(File.dirname(__FILE__) + "/../lib")
-require "gist"
-
-Gist.clear
-Gist.process_selection
-Gist.send(ARGV[0] == "private")
--- textmate/bundles/GitHub.tmbundle/Support/bin/send_multifile_gist.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-
-$:.unshift(File.dirname(__FILE__) + "/../lib")
-require "gist"
-
-Gist.send(ARGV[0] == "private")
