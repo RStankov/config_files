791384c61494d34b3295cefdb9063d9b48426563
Radoslav Stankov <rstankov@gmail.com>
2012-05-07 23:08:34 +0300

    Cleanup the installer

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,11 +2,6 @@ require 'lib/installer.rb'
 
 desc 'Install config files'
 task :default do
-  Dir['dot/*'].each do |file_name|
-    Installer.link file_name, :link_name => ".#{File.basename(file_name)}"
-  end
-
-  Dir['bin/*'].each do |file_name|
-    Installer.link file_name, :directory => File.join(ENV['HOME'], 'bin')
-  end
+  Installer.link('dot/*') { |file_name| ".#{file_name}" }
+  Installer.link('bin/*') { |file_name| File.join 'bin', file_name }
 end
--- lib/installer.rb
+++ lib/installer.rb
@@ -1,66 +1,41 @@
 class Installer
   class << self
-    def link(file_name, options = {})
-      link = self.new(file_name, options)
-
-      if link.already_exists?
-        link.replace
-      else
-        link.create
-      end
-    end
-
-    def link_dir_contents(from, to)
-      FileUtils.mkdir_p to
-
-      Dir["#{from}/*"].each do |file_name|
-        Installer.link file_name, :directory => to
-      end
-    end
-
-    def defaults(application, settings)
-      settings.each do |name, value|
-        value = case value
-          when true, false then "-bool #{value}"
-          when Integer     then "-int #{value}"
-          when Hash        then '-dict ' + value.map { |k,v| %("#{k}" "#{v}") }.join(' ')
-          else value
-        end
-
-        cmd = %Q(defaults write #{application} "#{name}" #{value})
-        puts cmd
-        system cmd
+    def link(files)
+      Dir[files].each do |file_path|
+        link_path = yield File.basename(file_path)
+        Link.new(file_path, link_path).ensure_link
       end
     end
   end
 
-  def initialize(file_name, options = {})
-    @file_name = file_name
-    @link_name = options.delete(:link_name) || File.basename(file_name)
-    @directory = options.delete(:directory) || ENV['HOME']
-  end
+  class Link
+    def initialize(file_path, link_path)
+      @file_path = file_path
+      @link_path = File.join ENV['HOME'], link_path
+    end
 
-  def already_exists?
-    File.exist? link_path
-  end
+    def ensure_link
+      remove if already_exists?
+      create
+    end
 
-  def replace
-    system "rm -rf #{e link_path}"
-    create
-  end
+    private
 
-  def create
-    puts "linking #{link_path}"
-    system "ln -s $PWD/#{e @file_name} #{e link_path}"
-  end
+    def already_exists?
+      File.exist? @link_path
+    end
 
-  private
+    def remove
+      system "rm -rf #{e @link_path}"
+    end
 
-  def link_path
-    File.join(@directory, @link_name)
-  end
+    def create
+      puts "linking #{@link_path}"
+      system "ln -s $PWD/#{e @file_path} #{e @link_path}"
+    end
 
-  def e(path)
-    path.gsub(/( |\(|\))/){ |s| '\\' + s }
+    def e(path)
+      path.gsub(/( |\(|\))/) { |s| '\\' + s }
+    end
   end
 end
