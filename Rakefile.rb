b7eb74be04b0f86c4ee897d0263ef57b258100b2
Radoslav Stankov <rstankov@gmail.com>
2013-12-05 21:26:29 +0200

    Have rake install task and default task will just lead to it

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,7 +1,9 @@
 require_relative 'lib/installer.rb'
 
 desc 'Install config files'
-task :default do
+task :install do
   Installer.link('dot/*') { |file_name| ".#{file_name}" }
   Installer.link('bin/*') { |file_name| File.join 'bin', file_name }
 end
+
+task default: :install
