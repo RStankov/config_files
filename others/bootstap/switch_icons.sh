df4f6e9cb6bc3b3e8cdd972119a3c0b14c7f6b55
Radoslav Stankov <rstankov@gmail.com>
2019-09-01 12:48:22 +0300

    iTerm has a nice icon now

--- others/bootstap/switch_icons.sh
+++ others/bootstap/switch_icons.sh
@@ -4,9 +4,6 @@
 cp ./icons/MacVim.icns /Applications/MacVim.app/Contents/Resources/
 cp ./icons/MacVim-generic.icns /Applications/MacVim.app/Contents/Resources/
 
-# Use Terminal icon for iTerm
-cp ./icons/iTerm.icns /Applications/iTerm.app/Contents/Resources/
-
 # Caffeine retina icon
 cp ./icons/Caffeine/active.png /Applications/Caffeine.app/Contents/Resources
 cp ./icons/Caffeine/inactive.png /Applications/Caffeine.app/Contents/Resources
