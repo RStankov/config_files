aece14d08712d438333d5d39de2ae765c9d8222e
Radoslav Stankov <rstankov@gmail.com>
2019-12-15 11:31:03 +0200

    Improve bootstrap scripts

--- others/bootstap/install.sh
+++ others/bootstap/install.sh
@@ -5,48 +5,22 @@ brew install wget
 brew install zsh
 brew install git
 brew install macvim
-brew install ruby-build
 brew install rbenv
 brew install node
 brew install nvm
+brew install yarn
 brew install imagemagick
 brew install tree
 brew install elixir
 brew install erlang
 brew install rust
+brew install ripgrep
 
 # Cask
 brew install caskroom/cask/brew-cask
 
 # Apps
-brew cask install alfred
-brew cask install dropbox
-brew cask install firefox
-brew cask install google-chrome
-brew cask install backblaze
-brew cask install slack
-brew cask install spotify
-brew cask install iTerm2
-brew cask install omnidisksweeper
-brew cask install vlc
-brew cask install fork
-brew cask install karabiner
-brew cask install cleanmymac
-brew cask install skype
 brew cask install virtualbox
-brew cask install bartender
-brew cask install atom
-brew cask install cloudapp
-brew cask install dash
-brew cask install paw
-brew cask install zoomus
-brew cask install vlc
-brew cask install slack
-brew cask install sketch
-brew cask install skype
-brew cask install caffeine
-brew cask install docker
-brew cask install bartender
 
 # Quick look
 brew cask install betterzipql
@@ -57,13 +31,3 @@ brew cask install qlstephen
 brew cask install quicklook-csv
 brew cask install quicklook-json
 brew cask install webpquicklook
-
-# Link Cask Apps to Alfred
-brew cask alfred link
-
-# cleanup
-brew cleanup --force
-rm -f -r /Library/Caches/Homebrew/*
-
-# Install libraries
-cargo install ripgrep
--- others/bootstap/switch_icons.sh
+++ others/bootstap/switch_icons.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Use more modern vim icon for MacVim
-cp ./icons/MacVim.icns /Applications/MacVim.app/Contents/Resources/
-cp ./icons/MacVim-generic.icns /Applications/MacVim.app/Contents/Resources/
-
 # Caffeine retina icon
 cp ./icons/Caffeine/active.png /Applications/Caffeine.app/Contents/Resources
 cp ./icons/Caffeine/inactive.png /Applications/Caffeine.app/Contents/Resources
