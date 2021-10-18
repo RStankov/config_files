7eb0c67182ff4e3eb9c73050040eeb08a01774eb
Radoslav Stankov <rstankov@gmail.com>
2021-10-18 09:22:38 +0300

    Remove Caffeine icon

--- others/bootstap/switch_icons.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Caffeine retina icon
-cp ./icons/Caffeine/active.png /Applications/Caffeine.app/Contents/Resources
-cp ./icons/Caffeine/inactive.png /Applications/Caffeine.app/Contents/Resources
-cp ./icons/Caffeine/highlightactive.png /Applications/Caffeine.app/Contents/Resources
-cp ./icons/Caffeine/highlighted.png /Applications/Caffeine.app/Contents/Resources
