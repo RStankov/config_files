081c98e81c930c407e36b2a5a37db5689511b233
Radoslav Stankov <rstankov@gmail.com>
2025-03-17 16:28:21 +0200

    Fix error
    execution error: System Events got an error: osascript is not allowed to send keystrokes.

--- bin/dev-producthunt.sh
+++ bin/dev-producthunt.sh
@@ -10,37 +10,45 @@ cd /Users/rstankov/Projects/producthunt
 docker-compose up -d --no-recreate
 
 osascript <<EOF
-tell application "iTerm"
-    activate
+tell application "iTerm2"
+    # Create a new window or use existing one
     if (count of windows) = 0 then
-        set newWindow to (create window with default profile)
-    else
-        set newWindow to current window
+        create window with default profile
     end if
 
-    tell newWindow
+    # Get the current window
+    set currentWindow to current window
+
+    # First tab
+    tell currentWindow
         create tab with default profile
         tell current session
             write text "cd /Users/rstankov/Projects/producthunt && bundle exec puma -C config/puma.rb -p 5051"
         end tell
     end tell
 
-    tell application "System Events" to keystroke "t" using {command down}
-    delay 0.2
-    tell current session of newWindow
-        write text "cd /Users/rstankov/Projects/producthunt/frontend && nvm use && RAILS_URL=http://localhost:5051 NODE_ENV=development nodemon server/index.ts"
+    # Second tab
+    tell currentWindow
+        create tab with default profile
+        tell current session
+            write text "cd /Users/rstankov/Projects/producthunt/frontend && nvm use && RAILS_URL=http://localhost:5051 NODE_ENV=development nodemon server/index.ts"
+        end tell
     end tell
 
-    tell application "System Events" to keystroke "t" using {command down}
-    delay 0.2
-    tell current session of newWindow
-        write text "cd /Users/rstankov/Projects/producthunt/frontend && nvm use && concurrently \"yarn graphql:codegen --errors-only --watch\""
+    # Third tab
+    tell currentWindow
+        create tab with default profile
+        tell current session
+            write text "cd /Users/rstankov/Projects/producthunt/frontend && nvm use && concurrently \"yarn graphql:codegen --errors-only --watch\""
+        end tell
     end tell
 
-    tell application "System Events" to keystroke "t" using {command down}
-    delay 0.2
-    tell current session of newWindow
-        write text "cd /Users/rstankov/Projects/producthunt"
+    # Fourth tab
+    tell currentWindow
+        create tab with default profile
+        tell current session
+            write text "cd /Users/rstankov/Projects/producthunt"
+        end tell
     end tell
 end tell
 EOF
